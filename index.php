e6380e020f6a158f98265be3992b4480bcf5afe5
Jevon Wright <jevon@jevon.org>
2014-03-03 03:47:32 -0800

    Fixing Google Analytics code.

--- index.php
+++ index.php
@@ -82,6 +82,12 @@ if (count($title) == 2) {
   _gaq.push(['_setDomainName', <?php echo json_encode(get_site_config('google_analytics_domain')); ?>]);
   _gaq.push(['_setAllowLinker', true]);
   _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 </script>
 </body>
 </html>
